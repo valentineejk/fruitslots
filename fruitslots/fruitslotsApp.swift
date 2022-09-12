2d93a80ef29d568e2a7692f43933620758a7636c
valentineejk <valentineejk@gmail.com>
2022-09-12 09:36:49 +0100

    Initial Commit

---
+++ fruitslots/fruitslotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  fruitslotsApp.swift
+//  fruitslots
+//
+//  Created by Valentineejk on 12/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct fruitslotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
